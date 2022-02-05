bdccaf7a3544edb1a39c8b195df46f4eaf60df9e
taras-svystun <tatarik.sv@gmail.com>
2022-02-05 17:21:30 +0200

    task 1

--- Intro-to-Deep-Learning/1.py
+++ Intro-to-Deep-Learning/1.py
@@ -1,5 +1,6 @@
 from tensorflow import keras
 from tensorflow.keras import layers
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -14,3 +15,29 @@ plt.rc('axes', labelweight='bold', labelsize='large',
 model = keras.Sequential([
     layers.Dense(units=1, input_shape=[3])
 ])
+
+print(red_wine.shape)
+# The target is 'quality'
+input_shape = [red_wine.shape[1] - 1]
+model = keras.Sequential([
+    layers.Dense(units=1, input_shape=input_shape)
+])
+w, b = model.weights
+print(w, b)
+
+# Additional
+model = keras.Sequential([
+    layers.Dense(units=1, input_shape=[1])
+])
+x = tf.linspace(-3, 3, 100)
+y = model.predict(x)
+
+plt.figure()
+plt.plot(x, y)
+plt.xlim(-3, 3)
+plt.ylim(-3, 3)
+plt.xlabel("Input x")
+plt.ylabel("Predicted y")
+w, b = model.weights
+plt.title(f"Weight : {round(float(w[0][0]), 3)}\nBias : {b[0]}")
+plt.show()
